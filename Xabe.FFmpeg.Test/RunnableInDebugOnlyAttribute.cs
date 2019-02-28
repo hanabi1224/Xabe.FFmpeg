c7b64644076d5f1f93587d5ba07cbfcff8b5178f
hanabi1224 <harlowmoo@gmail.com>
2019-02-28 19:37:55 +0800

    update

--- Xabe.FFmpeg.Test/RunnableInDebugOnlyAttribute.cs
+++ Xabe.FFmpeg.Test/RunnableInDebugOnlyAttribute.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics;
+﻿using System;
 using Xunit;
 
 namespace Xabe.FFmpeg.Test
@@ -11,5 +11,32 @@ namespace Xabe.FFmpeg.Test
             Skip = "Only running in interactive mode.";
 #endif
         }
+    }
+
+    public class SkipAppVeyorFactAttribute : FactAttribute
+    {
+        public SkipAppVeyorFactAttribute()
+        {
+            if (EnvironmentUtils.IsAppVeyorBuild)
+            {
+                Skip = "Skip on AppVeyor build";
+            }
+        }
+    }
+
+    public class SkipAppVeyorTheoryAttribute : TheoryAttribute
+    {
+        public SkipAppVeyorTheoryAttribute()
+        {
+            if (EnvironmentUtils.IsAppVeyorBuild)
+            {
+                Skip = "Skip on AppVeyor build";
+            }
+        }
+    }
+
+    public static class EnvironmentUtils
+    {
+        public static bool IsAppVeyorBuild => StringComparer.OrdinalIgnoreCase.Equals(Environment.GetEnvironmentVariable("APPVEYOR"), "true");
     }
 }
